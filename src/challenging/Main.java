8b1ace167272eb12ed0f0b37083347bd0232fdd9
mynameisdaun <daun9870.jung@gmail.com>
2021-10-30 13:50:46 +0900

    baby shark ing

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -1,88 +1,79 @@
-package clear;
+package challenging;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int row;
-    static int col;
+
     static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int answer = Integer.MIN_VALUE;
+    static BabyShark babyShark;
+    static List<Fish> fishList = new LinkedList<Fish>();
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        row = Integer.parseInt(str[0]);
-        col = Integer.parseInt(str[1]);
-        map = new int[row][col];
-        visited = new boolean[row][col];
+        int n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
 
-        for(int i = 0 ; i < row ; i ++) {
-            String[] colArr = br.readLine().split(" ");
-            for(int j = 0 ; j < col ; j++) {
-                map[i][j] = Integer.parseInt(colArr[j]);
+        //init
+        for(int i = 0 ; i < n ; i ++) {
+            String[] str = br.readLine().split(" ");
+            for(int j = 0 ; j < n ; j ++) {
+                map[i][j] = Integer.parseInt(str[j]);
+
+                if(map[i][j]!=9&&map[i][j]!=0) {
+                    fishList.add(new Fish(i, j, map[i][j]));
+                }
+
+                if(map[i][j]==9) {
+                    babyShark = new BabyShark(i, j, 2, 0);
+                }
+
             }
         }
+        // init end
 
-        for(int i = 0 ; i < row; i ++) {
-            for(int j = 0 ; j < col ; j ++) {
-                visited[i][j] = true;
-                DFS(1, map[i][j], i, j);
-                checkCross(map[i][j], i, j);
-                visited[i][j] = false;
-            }
-        }
-        System.out.println(answer);
-    }
-
-    static void DFS(int L, int S, int x, int y) {
-        if(L==4) {
-            answer = Math.max(S,answer);
-        } else {
-
-            for(int i = 0 ; i < 4; i ++ ) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx>=0&&nx<row&&ny>=0&&ny<col&&!visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    DFS(L+1, S+map[nx][ny],nx,ny);
-                    visited[nx][ny] = false;
+        Collections.sort(fishList, new Comparator<Fish>() {
+            @Override
+            public int compare(Fish o1, Fish o2) {
+                if(o1.size==o2.size) {
+                    return o1.x==o2.x ? o1.y-o2.y : o1.x-o2.x;
+                }else {
+                    return o1.size-o2.size;
                 }
             }
-        }
-    }
+        });
 
-    static void checkCross(int S, int x, int y) {
-        int lx = x-1; int ly = y;
-        int rx = x+1; int ry = y;
-        int dx = x;   int dy = y-1;
-        int ux = x;   int uy = y+1;
 
-        // ㅏ shape
-        if(ux>=0&&ux<row&&uy>=0&&uy<col&&rx>=0&&rx<row&&ry>=0&&ry<col&&dx>=0&&dx<row&&dy>=0&&dy<col) {
-            int shape_ㅏ = S + map[ux][uy]+map[rx][ry]+map[dx][dy];
-            answer = Math.max(shape_ㅏ,answer);
-        }
-        // ㅗ shape
-        if(ux>=0&&ux<row&&uy>=0&&uy<col&&rx>=0&&rx<row&&ry>=0&&ry<col&&lx>=0&&lx<row&&ly>=0&&ly<col) {
-            int shape_ㅗ= S + map[ux][uy]+map[rx][ry]+map[lx][ly];
-            answer = Math.max(shape_ㅗ,answer);
-        }
-        // ㅜ shape
-        if(dx>=0&&dx<row&&dy>=0&&dy<col&&rx>=0&&rx<row&&ry>=0&&ry<col&&lx>=0&&lx<row&&ly>=0&&ly<col) {
-            int shape_ㅜ= S + map[dx][dy]+map[rx][ry]+map[lx][ly];
-            answer = Math.max(shape_ㅜ,answer);
-        }
-        // ㅓ  shape
-        if(dx>=0&&dx<row&&dy>=0&&dy<col&&ux>=0&&ux<row&&uy>=0&&uy<col&&lx>=0&&lx<row&&ly>=0&&ly<col) {
-            int shape_ㅓ= S + map[dx][dy]+map[ux][uy]+map[lx][ly];
-            answer = Math.max(shape_ㅓ,answer);
-        }
+
+
+
     }
 
 
+    static class BabyShark {
+        int x;
+        int y;
+        int size;
+        int eat;
+        public BabyShark(int x, int y, int size, int eat) {
+            this.x=x;
+            this.y=y;
+            this.size=size;
+            this.eat=eat;
+        }
+    }
+
+    static class Fish {
+        int x;
+        int y;
+        int size;
+        public Fish(int x, int y, int size) {
+            this.x=x;
+            this.y=y;
+            this.size=size;
+        }
+    }
 }
