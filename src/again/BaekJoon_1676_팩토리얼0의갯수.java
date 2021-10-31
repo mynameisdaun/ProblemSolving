cfc351c7e2517f7c1f7c57d01695bcc2ff3e3748
mynameisdaun <daun9870.jung@gmail.com>
2021-11-01 06:47:16 +0900

    팩토리얼 0의 갯수

---
+++ src/again/BaekJoon_1676_팩토리얼0의갯수.java
@@ -0,0 +1,29 @@
+package challenging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static long[] dp = new long[501];
+    static int answer = 0 ;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0 ;
+
+        while(N >= 5){
+            count += N/5;
+            N /=5;
+        }
+
+        System.out.println(count);
+    }
+
+}
+
+
+
