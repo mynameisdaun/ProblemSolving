2f02436cc1c469a924a4ef721d30a353b5627317
mynameisdaun <daun9870.jung@gmail.com>
2021-10-31 19:24:23 +0900

    나는야 포켓몬 마스터 이다솜 ..

---
+++ src/clear/BaekJoon_1620_나는야포켓몬마스터이다솜.java
@@ -0,0 +1,44 @@
+package challenging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int numberOfPoketmon = Integer.parseInt(str[0]);
+        int numberOfQuiz = Integer.parseInt(str[1]);
+
+        Map<String, Integer> map_name_number = new HashMap<String, Integer>();
+        Map<Integer, String> map_number_name = new HashMap<Integer, String>();
+
+        for(int i = 1 ; i <= numberOfPoketmon ; i ++) {
+            String nameOfPoketmon = br.readLine();
+            map_name_number.put(nameOfPoketmon,i);
+            map_number_name.put(i, nameOfPoketmon);
+        }
+
+        for(int i = 0 ; i < numberOfQuiz ; i ++) {
+            String quiz = br.readLine();
+            char firstCharacter = quiz.charAt(0);
+
+            if(Character.isDigit(firstCharacter)) {
+                System.out.println(map_number_name.get(Integer.parseInt(quiz)));
+            } else {
+                System.out.println(map_name_number.get(quiz));
+            }
+
+        }
+
+
+    }
+
+
+}
+
+
