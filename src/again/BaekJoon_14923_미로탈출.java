1dd0cba6cd9cbffa8669e79290402aff4901cdff
mynameisdaun <daun9870.jung@gmail.com>
2021-11-15 18:59:36 +0900

    벽 부수고 이동하기

--- src/again/BaekJoon_14923_미로탈출.java
+++ src/again/BaekJoon_14923_미로탈출.java
@@ -1,4 +1,4 @@
-//package challenging;
+package again;//package challenging;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BaekJoon_14923_미로탈출 {
     static int N, M, Hx, Hy, Ex, Ey, ans = Integer.MAX_VALUE;
     static int map[][];
     static boolean[][][] visited;
