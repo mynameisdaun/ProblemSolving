b28d37cd7ba2b312a0b8d750faea4740f5cca581
mynameisdaun <daun9870.jung@gmail.com>
2022-03-14 00:13:38 +0900

    조합구하기

---
+++ src/clear/조합구하기.java
@@ -0,0 +1,38 @@
+package challenging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = input1[0];
+        int M = input1[1];
+        main.DFS(0, 1, N, M, new boolean[N+1], new int[M+1]);
+    }
+
+    void DFS(int L, int S, int N, int M, boolean[] check, int[] print) {
+        if(L==M) {
+            for(int i = 0 ; i < M ; i ++) {
+                System.out.print(print[i]+" ");
+            }
+            System.out.println();
+        } else {
+            for(int i = S ; i <= N ; i ++) {
+                if(!check[i] && L < i) {
+                    check[i] = true;
+                    print[L] = i;
+                    DFS(L+1, i, N, M, check, print);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+
+
+}
