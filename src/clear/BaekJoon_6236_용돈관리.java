3e2c4f593c9be4c8845836f2486b0a60e5b89c37
mynameisdaun <daun9870.jung@gmail.com>
2022-03-21 23:13:43 +0900

    용돈관리

---
+++ src/clear/BaekJoon_6236_용돈관리.java
@@ -0,0 +1,51 @@
+package challenging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = input1[0];
+        int M = input1[1];
+        int[] arr = new int[N];
+        int max = Integer.MIN_VALUE;
+
+
+        for(int i = 0 ; i < N ; i ++) {
+            arr[i]=Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        int start = max ;
+        int end = (int) 1e9;
+        int answer = 0;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int 잔액 = 0 ;
+            int count = 0 ;
+
+            for(int i = 0 ; i < N ; i ++) {
+                if(잔액 < arr[i]) {
+                    ++count;
+                    잔액 = mid - arr[i];
+                } else {
+                    잔액-=arr[i];
+                }
+            }
+
+            if(count > M) {
+                start = mid + 1;
+            } else {
+                answer=mid;
+                end = mid -1 ;
+            }
+        }
+        System.out.println(answer);
+    }
+}
