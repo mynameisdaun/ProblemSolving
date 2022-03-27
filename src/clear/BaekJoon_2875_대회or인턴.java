3d7a4c5801e08dc62f2a928bead6c2581aa2748a
mynameisdaun <daun9870.jung@gmail.com>
2022-03-27 17:23:17 +0900

    대회 or 인턴

---
+++ src/clear/BaekJoon_2875_대회or인턴.java
@@ -0,0 +1,26 @@
+package challenging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = input[0];int M = input[1];int K = input[2];
+        int answer = 0;
+
+        while(N>=2&&M>=1) { N-=2;M--;answer++;}
+        //System.out.println("N: "+N+" M: "+M+" answer: "+answer);
+        if(N+M < K) {
+            //이 부분이 문제인 것 같당!
+            int 빼야하는팀 = (int) Math.ceil((K-N-M) * 1.0 /3);
+            System.out.println(Math.max(answer-빼야하는팀,0));
+        } else {
+            System.out.println(Math.max(answer,0));
+        }
+    }
+}
+
