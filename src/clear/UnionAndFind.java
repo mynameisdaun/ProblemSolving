f858b803ecbe432a62fcd448da35291660229e2a
mynameisdaun <daun9870.jung@gmail.com>
2022-05-04 22:25:37 +0900

    union and find

--- src/clear/UnionAndFind.java
+++ src/clear/UnionAndFind.java
@@ -1,10 +1,9 @@
-package challenging;
+package clear;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
-public class Main {
+public class UnionAndFind {
     private static int INF = 987654321;
 
     public static void main(String[] args) throws IOException {
