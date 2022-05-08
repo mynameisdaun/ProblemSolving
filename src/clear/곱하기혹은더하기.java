064cbf7e1276dc78e58a0709ee4dd026914d466c
mynameisdaun <daun9870.jung@gmail.com>
2022-05-08 09:38:21 +0900

    곱하기혹은더하기

---
+++ src/clear/곱하기혹은더하기.java
@@ -0,0 +1,28 @@
+package challenging;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    private static int INF = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        //String s = "02984";
+        String s = "567";
+//313
+        if(s.length()==1) {
+            System.out.println(s);
+            return;
+        }
+        int[] arr = Arrays.stream(s.split("")).mapToInt(Integer::parseInt).toArray();
+        int answer = arr[0];
+        for(int i = 1 ; i < arr.length ; i++) {
+            if(answer<=1||arr[i]<=1) {
+                answer += arr[i];
+                continue;
+            }
+            answer *= arr[i];
+        }
+        System.out.println(answer);
+    }
+}
