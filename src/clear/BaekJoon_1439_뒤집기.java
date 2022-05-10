62b949c848422b120d752994917e5d8cba20ac6b
mynameisdaun <daun9870.jung@gmail.com>
2022-05-10 22:59:03 +0900

    뒤집기

--- src/clear/BaekJoon_1439_뒤집기.java
+++ src/clear/BaekJoon_1439_뒤집기.java
@@ -1,10 +1,10 @@
-package challenging;
+package clear;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class BaekJoon_1439_뒤집기 {
 
     public static void main(String[] args) throws IOException {
          //String s = "0001100";
