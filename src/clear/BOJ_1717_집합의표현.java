981d446e68ceeaa09a62758dd62e277d194fa3b9
mynameisdaun <daun9870.jung@gmail.com>
2022-09-11 22:55:08 +0900

    집합의 표현

--- src/clear/BOJ_1717_집합의표현.java
+++ src/clear/BOJ_1717_집합의표현.java
@@ -1,11 +1,11 @@
-package challenging;
+package clear;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class BOJ_1717_집합의표현 {
 
     public static void union(int[] parent, int a, int b) {
         int a_parent = find(parent, a);
