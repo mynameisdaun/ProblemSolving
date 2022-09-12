987c9cfd18f9b66fca652467e2ae765f6599ac83
mynameisdaun <daun9870.jung@gmail.com>
2022-09-13 00:42:38 +0900

    0913 ps

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -4,46 +4,75 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
+    static int start;
+    static int end;
+
+    public static void union(int[] parent, int a, int b) {
+        int a_parent = find(parent, a);
+        int b_parent = find(parent, b);
+
+        if (a_parent < b_parent)
+            parent[b_parent] = a_parent;
+        else
+            parent[a_parent] = b_parent;
+    }
+
+    public static int find(int[] parent, int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent, parent[i]);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] liquids = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int A = 0;
-        int B = 0;
-        int answer = Integer.MAX_VALUE;
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (int i = 0; i < n - 1; i++) {
-            int left = i;
+        int n = input[0];
+        int m = input[1];
 
-            int start = i + 1;
-            int end = n - 1;
+        int[] parents = new int[n + 1];
 
-
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                int tmp = liquids[left] + liquids[mid];
-                //System.out.println("left : " + left + " start: " + start + " end: " + end + "" + " mid: " + mid + " tmp: " + tmp);
-                if (tmp == 0) {
-                    System.out.print(liquids[left] + " " + liquids[mid]);
-                    return;
-                }
-                if (tmp > 0) {
-                    end = mid-1;
-                } else {
-                    start = mid+1;
-                }
-                if (Math.abs(tmp) < answer) {
-                    A = left;
-                    B = mid;
-                    answer = Math.abs(tmp);
-                }
-            }
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
         }
 
-        System.out.println(liquids[A]+" "+liquids[B]);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> b.w - a.w);
+
+
+        for (int i = 0; i < m; i++) {
+            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            pq.add(new Node(input[0], input[1], input[2]));
+        }
+
+        input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        start = input[0];
+        end = input[1];
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            int s = now.s;
+            int e = now.e;
+            union(parents, s, e);
+            if (find(parents, start) == find(parents, end)) {
+                System.out.println(now.w);
+                return;
+            }
+        }
+    }
+
+    static class Node {
+        private int s;
+        private int e;
+        private int w;
+
+        public Node(int s, int e, int w) {
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
     }
 }
 
