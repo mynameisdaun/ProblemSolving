274d0064232bd0721c860e602612dd256041d00c
mynameisdaun <daun9870.jung@gmail.com>
2022-09-17 23:14:30 +0900

    0917 ps

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -3,89 +3,81 @@ package challenging;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
-    public static void union(int[] parent, int a, int b, Map<Integer, Set<Integer>> network) {
-        int a_parent = find(parent, a);
-        int b_parent = find(parent, b);
+    static int N, v, e, d, parent[];
+    static long distance[];
 
-        Set<Integer> aNetwork = network.get(a_parent);
-        Set<Integer> bNetwork = network.get(b_parent);
-
-        if (a_parent < b_parent) {
-            bNetwork.forEach(n-> parent[n] = a_parent);
-            aNetwork.addAll(bNetwork);
-            network.put(a_parent, aNetwork);
-            parent[b_parent] = a_parent;
-        } else {
-            aNetwork.forEach(n-> parent[n] = b_parent);
-            bNetwork.addAll(aNetwork);
-            network.put(b_parent, bNetwork);
-            parent[a_parent] = b_parent;
+    public static void union(int a, int b) {
+        if (a != b) {
+            if (a > b) {
+                parent[b] = a;
+            } else {
+                parent[a] = b;
+            }
         }
-
     }
 
-    public static int find(int[] parent, int i) {
-        if (parent[i] == i)
-            return i;
-        return parent[i] = find(parent, parent[i]);
+    public static int find(int n) {
+        return parent[n] == n ? n : (parent[n] = find(parent[n]));
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tcase = Integer.parseInt(br.readLine());
-        while (tcase-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-
-            int[] parents = new int[200000];
-            for (int i = 0; i < parents.length; i++) {
-                parents[i] = i;
-            }
-
-            Map<String, Integer> map = new HashMap<>();
-            Map<Integer, Set<Integer>> network = new HashMap<>();
-
-            for (int i = 0; i < n; i++) {
-                String[] input = br.readLine().split(" ");
-                String a = input[0];
-                String b = input[1];
-
-                Integer indexA = map.get(a);
-                if(indexA == null) {
-                    indexA = map.size();
-                    map.put(a, indexA);
-                    Set<Integer> aSet = new HashSet<>();
-                    aSet.add(indexA);
-                    network.put(indexA, aSet);
-                }
-
-                Integer indexB = map.get(b);
-                if(indexB == null) {
-                    indexB = map.size();
-                    map.put(b, indexB);
-                    Set<Integer> bSet = new HashSet<>();
-                    bSet.add(indexB);
-                    network.put(indexB, bSet);
-                }
-
-                int a_parent = find(parents, indexA);
-                int b_parent = find(parents, indexB);
-
-                if (a_parent != b_parent) {
-                    union(parents, indexA, indexB, network);
-                }
-
-                int idx = Math.min(a_parent, b_parent);
-                System.out.println(network.get(idx).size());
-            }
-
+        N = parseInt(br.readLine());
+        parent = new int[N + 1];
+        distance = new long[N + 1];
+        for (int i = 1; i <= N; i++) parent[i] = i;
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<List<Integer>>((a, b) -> a.get(0) - b.get(0));
+        for (int t = 0; t < N; t++) {
+            pq.offer(
+                    Arrays.stream(br.readLine().split(" "))
+                            .mapToInt(Integer::parseInt)
+                            .boxed()
+                            .collect(Collectors.toList())
+            );
         }
+        while (!pq.isEmpty()) {
+            List<Integer> list = pq.poll();
+            v = find(list.get(0));
+            for (int i = 1; i < list.size() - 1; i += 2) {
+                e = find(list.get(i));
+                System.out.println("v: "+v+" e: "+e);
+                d = list.get(i + 1);
+                if (v != e) {
+                    distance[e] = Math.max(distance[v] + d, distance[e]);
+                    for (int j = 1; j <= N; j++) {
+                        System.out.print(distance[j]+" ");
+                    }
+                    System.out.println();
+                    union(v, e);
+                }
+            }
+        }
+        System.out.println(distance[N]);;
     }
 }
 /*
-
-
+5
+1 3 2 -1
+5 4 6 -1
+3 1 2 4 3 -1
+2 4 4 -1
+4 2 4 3 3 5 6 -1
  */
+/*
+5
+1 5 1 -1
+5 1 1 4 10 -1
+4 3 10 5 10 -1
+3 2 10 4 10 -1
+2 3 10 -1
+ */
+
