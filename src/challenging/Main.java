22f5a7b892a9be1ead45c99ab465507f234ea485
mynameisdaun <daun9870.jung@gmail.com>
2022-10-08 23:36:31 +0900

    221008 ps

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -1,46 +1,52 @@
 package challenging;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.IOException;
+import java.util.Stack;
 
 public class Main {
-    static int n, seq[];
-    static long answer = 0, memo[][];
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        seq = new int[n];
-        String[] input = br.readLine().split(" ");
-        memo = new long[n][21];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
-            seq[i] = Integer.parseInt(input[i]);
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            sb.append(solve(br.readLine())).append('\n');
         }
-        System.out.println(dfs(0, seq[0]));
+
+        System.out.println(sb);
     }
 
-    static long dfs(int L, int S) {
-        if (memo[L][S] != -1) {
-            return memo[L][S];
-        }
-        if (L == n - 2) {
-            if (S == seq[n-1]) {
-                return memo[L][S] = 1;
-            } else {
-                return memo[L][S] = 0;
+    public static String solve(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            }
+            else if (stack.empty()) {
+                return "NO";
+            }
+            // 그 외의 경우 stack 원소를 pop 한다.
+            else {
+                stack.pop();
             }
         }
-        long sum = 0;
-        if (S + seq[L + 1] <= 20) {
-            sum += (memo[L + 1][S + seq[L + 1]] = dfs(L + 1, S + seq[L + 1]));
+
+
+        if (stack.empty()) {
+            return "YES";
         }
-        if (S - seq[L + 1] >= 0) {
-            sum += (memo[L + 1][S - seq[L + 1]] = dfs(L + 1, S - seq[L + 1]));
+        else {
+            return "NO";
         }
-        return memo[L][S] = sum;
     }
 }
