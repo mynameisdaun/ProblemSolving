683321e3404ce7b75d93f6dc511a04302e469110
mynameisdaun <daun9870.jung@gmail.com>
2022-10-21 01:06:35 +0900

    221021 boj 게임개발

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -10,77 +10,84 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int n = input[0], k = input[1], r = input[2];
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-        List<List<List<int[]>>> road = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            road.add(new ArrayList<>());
-            for (int j = 0; j <= n; j++) {
-                road.get(i).add(new ArrayList<>());
+        int n = Integer.parseInt(br.readLine());
+        int[] inDegree = new int[n + 1];
+        int[] times = new int[n + 1];
+        Map<Integer, Integer> map = new HashMap<>();
+        boolean[][] board = new boolean[n + 1][n + 1];
+        boolean[] visited = new boolean[n + 1];
+        for (int i = 0; i < n; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+            times[i + 1] = input[0];
+            for (int j = 1; j < input.length; j++) {
+                if (input[j] == -1) break;
+                board[input[j]][i + 1] = true;
+                inDegree[i + 1]++;
             }
         }
 
-        for (int i = 0; i < r; i++) {
-            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int x1 = input[0], y1 = input[1], x2 = input[2], y2 = input[3];
-            road.get(x1).get(y1).add(new int[]{x2, y2});
-            road.get(x2).get(y2).add(new int[]{x1, y1});
-        }
-
-        int[][] board = new int[n + 1][n + 1];
-
-        for (int i = 0; i < k; i++) {
-            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            board[input[0]][input[1]] = 1;
-        }
-
-        int answer = 0;
+        //번호, 건물짓는데 걸리는 시간, 이제까지 걸린 시간
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (a[1]+a[2]) - (b[1]+b[2]));
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (board[i][j] == 1) {
-                    boolean[][] visited = new boolean[n + 1][n + 1];
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j});
-                    visited[i][j] = true;
-                    int cow = 0;
-                    while (!queue.isEmpty()) {
-                        int[] node = queue.poll();
-                        int x = node[0];
-                        int y = node[1];
-                        if (board[x][y] == 1) {
-                            cow++;
-                        }
-                        for (int l = 0; l < 4; l++) {
-                            int nx = x+dx[l];
-                            int ny = y+dy[l];
-                            if(nx>=1&&nx<=n&&ny>=1&&ny<=n&&!visited[nx][ny]&&noBridge(x,y,nx,ny,road)) {
-                                visited[nx][ny]=true;
-                                queue.offer(new int[]{nx, ny});
-                            }
-                        }
+            if (inDegree[i] == 0) {
+                queue.offer(new int[]{i, times[i], 0});
+                visited[i] = true;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int vertex = node[0];
+            int time = node[1];
+            int accumulateTime = node[2];
+            map.put(vertex, time + accumulateTime);
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i]&&board[vertex][i]) {
+                    if (--inDegree[i] == 0) {
+                        queue.offer(new int[]{i, times[i], time+accumulateTime});
+                        visited[i]=true;
                     }
-                    answer += (k - cow);
                 }
             }
         }
-        System.out.println(answer / 2);
-    }
-    public static boolean noBridge(int x1, int y1, int x2, int y2, List<List<List<int[]>>> road) {
-        if(road.get(x1).get(y1).isEmpty()) {
-            return true;
-        } else {
-            return road.get(x1).get(y1).stream()
-                    .filter( r ->r[0]==x2&&r[1]==y2).count() == 0;
+        for (int i = 1; i <= n ; i++) {
+            System.out.println(map.get(i));
         }
     }
-    /*
-    0 0 0
-
-    0 0 0
-      | |
-    0 0-0
-     */
 }
+/*
+4
+100(1) - 1(2) - 3 - 1
+103  -----------|
+
+4
+100 2 3 4 -1
+10 -1
+30 4 -1
+50 -1
+
+4
+100 -1
+1 1 -1
+3 1 2 4 -1
+103 -1
+
+5
+100 -1
+90 -1
+80 -1
+70 -1
+60 1 -1
+
+
+5
+10 -1
+10 1 -1
+4 1 -1
+4 3 1 -1
+3 3 -1
+
+ */
+
 
---
+++ src/clear/BOJ_소.java
@@ -0,0 +1,17 @@
+package clear;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_소 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("1"=="1");
+    }
+
+}
+
