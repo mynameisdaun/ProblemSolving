b115180ec64fe2ba9b446fb6edc14d3ef8445233
mynameisdaun <daun9870.jung@gmail.com>
2022-11-04 10:17:48 +0900

    221104 캠프 준비

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -7,21 +7,39 @@ import java.util.*;
 
 public class Main {
 
+    static int n, l, r, x, target, cb[], arr[], answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.stream(arr).forEach(a->map.put(a,0));
-        int arrow = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(map.get(arr[i])>0) {
-                map.put(arr[i], map.get(arr[i])-1);
-            } else {
-                arrow++;
-            }
-            map.put(arr[i]-1,map.getOrDefault(arr[i]-1,0)+1);
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        n = input[0];
+        l = input[1];
+        r = input[2];
+        x = input[3];
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        if(n==1) {
+            System.out.println(0);
+            return;
         }
-        System.out.println(arrow);
+        Arrays.sort(arr);
+
+        for (int i = 2; i <= n ; i++) {
+            cb = new int[i];
+            target = i;
+            dfs(0,0);
+        }
+        System.out.println(answer);
+    }
+
+    public static void dfs(int d, int s) {
+        if (d == target) {
+            int sum = Arrays.stream(cb).sum();
+            if (sum >= l && sum <= r && cb[d - 1] - cb[0] >= x) answer++;
+        } else {
+            for (int i = s; i < n; i++) {
+                cb[d] = arr[i];
+                dfs(d + 1, i + 1);
+            }
+        }
     }
 }
