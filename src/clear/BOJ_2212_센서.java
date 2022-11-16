97fad744ac75239125b6d8be9b682b57800a49c3
mynameisdaun <daun9870.jung@gmail.com>
2022-11-16 22:42:30 +0900

    221116 2212 센서

--- src/clear/BOJ_2212_센서.java
+++ src/clear/BOJ_2212_센서.java
@@ -1,4 +1,4 @@
-package challenging;
+package clear;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-public class Main {
+public class BOJ_2212_센서 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
