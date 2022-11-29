fb82daa42a8b7dad6e69bc63b91ad245668d9c6e
mynameisdaun <daun9870.jung@gmail.com>
2022-11-29 22:26:51 +0900

    221129 ps

--- src/challenging/Main.java
+++ src/challenging/Main.java
@@ -2,8 +2,10 @@ package challenging;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
 
